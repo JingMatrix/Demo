298bf3958f580e708f6cf466afd1c78c560797a5
JingMatrix <jingmatrix@gmail.com>
2025-08-16 21:09:18 +0200

    Refresh test result via double-clicking

--- app/src/main/java/org/matrix/demo/MainActivity.kt
+++ app/src/main/java/org/matrix/demo/MainActivity.kt
@@ -2,12 +2,18 @@ package org.matrix.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import org.matrix.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var doubleClick = false
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +23,24 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         binding.sampleText.text = stringFromJNI()
+
+        binding.root.setOnClickListener {
+            if (doubleClick) {
+                // Double click
+                binding.sampleText.text = stringFromJNI()
+                Toast.makeText(this, "result updated", Toast.LENGTH_SHORT).show()
+                doubleClick = false
+            } else {
+                // Single click
+                doubleClick = true
+                handler.postDelayed({
+                    if (doubleClick) {
+                        Toast.makeText(this, "double click to refresh result", Toast.LENGTH_SHORT).show()
+                        doubleClick = false
+                    }
+                }, 200) // Double click time window
+            }
+        }
     }
 
     /**
